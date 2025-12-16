1f9e11141bd6f2c8dc85106671f93246f99deaf6
Nadeef Muttaqin Chowdhury <nadeefchowdhury@gmail.com>
2025-12-16 17:30:40 +0600

    Create 2308083-4.c

---
+++ Practice 2/2308083-4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+int year;
+scanf("%d", &year);
+if(year%4==0){
+    if(year%400==0){
+    printf("Leap year");
+    }
+    else{
+        if(year%100==0){
+            printf("Not leap year");
+        }
+        else{
+            printf("Leap year");
+        }
+    }
+    }
+else{
+    printf("Not leap year");
+}
+
+
+
+return 0;
+}
