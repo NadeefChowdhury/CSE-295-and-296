8564ddf9bcc4a98dfb91d8a75a315ab8affdc40e
Nadeef Muttaqin Chowdhury <nadeefchowdhury@gmail.com>
2025-12-29 19:57:17 +0600

    Create 5(custom).c

---
+++ Practice 3/5(custom).c
@@ -0,0 +1,23 @@
+/*
+For printing
+*
+**
+***
+****
+
+
+*/  
+
+
+#include <stdio.h>
+
+int main() {
+    for(int i =1; i<5; i++){
+        for(int j =0; j<i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
