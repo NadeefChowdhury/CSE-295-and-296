d2de054ccb90e9bdc9f5882d05b3dc4e33e80cee
Nadeef Muttaqin Chowdhury <nadeefchowdhury@gmail.com>
2025-12-29 19:44:22 +0600

    Create 2308083-3.c

---
+++ Practice 3/2308083-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, notprime=0;
+    scanf("%d", &n);
+    for(int i=2; i<=(n/2); i++){
+        if(n%i==0){
+            notprime=1;
+            break;
+        }
+    }
+    if(notprime == 0) printf("Prime number");
+    else printf("Not prime number");
+    return 0;
+}
