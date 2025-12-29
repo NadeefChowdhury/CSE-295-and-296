11259bf7bbb86f9c0a4593612273ae789adabe57
Nadeef Muttaqin Chowdhury <nadeefchowdhury@gmail.com>
2025-12-29 20:08:14 +0600

    Create 2308083-6.c

---
+++ Practice 3/2308083-6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    for(int i =5; i>=1; i--){
+        for(int j =1; j<i; j++){
+            printf(" ");
+        }
+        for(int k=1; k<=9-(i-1)*2; k++){
+            printf("*");
+        }
+        for(int j =1; j<i; j++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
